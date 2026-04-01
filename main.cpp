e03deb4808b70320e66c0a7538b790089753c5ea
yooso <yoosoo27@snu.ac.kr>
2026-04-01 16:49:02 +0900

    정유수

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
 int N,M;
 vector<int> vec;
-
 void func(int cnt) {
+    if(cnt == M) {
+        for(int i = 0; i < M; i++) {
+            cout << vec[i];
+            if(i < M-1) cout << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
-
-
-
-
-
-
+    int start = vec.empty() ? 1 : vec.back() + 1;
+    for(int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
-
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
